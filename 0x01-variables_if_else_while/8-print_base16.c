1e855041117f556b7b6d2bc5a58e5692a429af11
MachinEmmus <emonsalvep38@gmail.com>
2019-09-20 04:32:09 +0000

    Something preocupations

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+char y;
+for (x = 48; x < 58; x++)
+putchar(x);
+for (y = 97; y < 103; y++)
+putchar(h);
+putchar('\n');
+return (0);
+}
