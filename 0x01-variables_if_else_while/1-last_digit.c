369846878306e2e4618cffa4826fc917c4387f98
MachinEmmus <55121296+MachinEmmus@users.noreply.github.com>
2019-09-20 16:22:41 -0500

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,9 +9,7 @@
 int main(void)
 {
 int n;
-
 srand(time(0));
-
 n = rand() - RAND_MAX / 2;
 if ((n % 10) < 6 && (n % 10) != 0)
 {
@@ -21,7 +19,7 @@ else if ((n % 10) == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, (n % 10));
 }
-else if ((n % 10) > 5)
+else
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10)); 
 }
