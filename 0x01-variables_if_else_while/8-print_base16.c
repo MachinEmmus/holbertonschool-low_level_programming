e31ac4b9d481b922bc8dc4beaf6f74a8244ddea4
MachinEmmus <emonsalvep38@gmail.com>
2019-09-20 16:49:52 -0500

    solution errors in 8 file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 {
 char x;
 char y;
-for (x = '48'; x < '58'; x++)
+for (x = 48; x < 58; x++)
 putchar(x);
-for (y = '97'; y < '103'; y++)
+for (y = 97; y < 103; y++)
 putchar(y);
 putchar('\n');
 return (0);
