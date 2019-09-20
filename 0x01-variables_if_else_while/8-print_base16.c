ac2c7522c78ba16222069bdf3135861d5b52965a
MachinEmmus <emonsalvep38@gmail.com>
2019-09-20 16:47:48 -0500

    add 8 files in his solution

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@ int main(void)
 {
 char x;
 char y;
-for (x = 48; x < 58; x++)
+for (x = '48'; x < '58'; x++)
 putchar(x);
-for (y = 97; y < 103; y++)
-putchar(h);
+for (y = '97'; y < '103'; y++)
+putchar(y);
 putchar('\n');
 return (0);
 }
