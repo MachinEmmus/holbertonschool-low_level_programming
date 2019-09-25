295cec8c7a8013b1668647a1bf4cffd11cc082a8
MachinEmmus <emonsalvep38@gmail.com>
2019-09-24 20:36:39 -0500

    Again fixed errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * print_to_98 - print the number 98
  * @x: First operand
  *
-*/
+ */
 void print_to_98(int x)
 {
-
 if (x < 98)
 {
 for (; x <= 98; x++)
@@ -25,7 +23,7 @@ for (; x >= 98; x--)
 {
 if (x == 98)
 {
-printf("%d", n);
+printf("%d", x);
 }
 else
 printf("%d, ", x);
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * times_table - writes the character c to stdout
+ * times_table - tables of multiplicate
  */
 void times_table(void)
 {
-int x = 0, y = 0, z;
-for (; x <= 9; x++)
+int x, y , z;
+for (x = 0; x <= 9; x++)
 {
-for (; y <= 9; y++)
+for (y = 0; y <= 9; y++)
 {
 z = x * y;
 if (z < 10)
