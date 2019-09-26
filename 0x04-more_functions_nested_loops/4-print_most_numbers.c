8b27f6973e95af9b5f0291285ac615ebc67ceb8b
MachinEmmus <emonsalvep38@gmail.com>
2019-09-26 15:53:45 -0500

    Esta vez todo estara bien

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,16 @@
 
 /**
  * print_most_numbers - print numbers distint 2y4
- * 
  * Return: void
  */
 
 void print_most_numbers(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-		if (i != 2 && i != 4)
-			_putchar('0' + i);
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i < 10; i++)
+{
+	if (i != 2 && i != 4)
+		_putchar('0' + i);
+}
+_putchar('\n');
 }
