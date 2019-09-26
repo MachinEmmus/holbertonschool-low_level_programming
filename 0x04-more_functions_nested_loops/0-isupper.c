445c27ff84ea9eb55916328b1411f1bc904461bf
MachinEmmus <emonsalvep38@gmail.com>
2019-09-26 12:39:16 -0500

    This the exervise and first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Check if a number is greater than 0
+ * @c: The number to be checked
+ *
+ * Return: 1 if the letter is uppercase. 0 otherwise
+ */
+
+int _isupper(int c)
+{   
+	if (c > 64 && c < 91)
+		return (1);
+	return (0);
+}
