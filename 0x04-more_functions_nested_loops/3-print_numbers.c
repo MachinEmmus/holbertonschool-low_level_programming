d7d485f5760b0c0438f7086dcc28a5762a64ba04
MachinEmmus <emonsalvep38@gmail.com>
2019-09-26 15:16:38 -0500

    portatese serio intranet

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 
 /**
- * print_numbers - print numbers between 1-9
+ * print_numbers - print numbers between 1 to 9
+ * Return: void
 */
 
 void print_numbers(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-		_putchar('0' + i);
-	}
-	_putchar('\n');
- }
+int i;
+for (i = 0; i < 10; i++)
+{
+	_putchar('0' + i);
+}
+_putchar('\n');
+}
 
