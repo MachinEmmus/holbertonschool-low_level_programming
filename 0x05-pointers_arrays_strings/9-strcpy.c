eaeab8bd8ee97843141b70110cc7d0e688f07f35
MachinEmmus <emonsalvep38@gmail.com>
2019-10-01 23:09:23 -0500

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,18 @@
-
+/**
+ * *_strcpy - funtion 
+ * @dest: first parametre
+ * @src: First parametre
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int x, z;
+x = 0;
+	while (src[x] != '\0')
+		x++;
+	for (z = 0; z <= x; z++)
+		dest[z] = src[z];
+	
+	return (dest);
+}
