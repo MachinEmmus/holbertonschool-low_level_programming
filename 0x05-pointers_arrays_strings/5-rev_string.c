06a05842ef2967edd632f16131dd6a45b6332d28
MachinEmmus <emonsalvep38@gmail.com>
2019-10-02 18:44:38 -0500

    I try solved this code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
 /**
- * rev_string - string invert
- * @s: is a string
- * Return: void
- */
-
+* rev_string - invert the string
+* @s: Strign and Pointer
+* Return: Void.
+*
+*/
 void rev_string(char *s)
 {
-int x, y, z;
-x = 0;
-char orden, desorden;
-	while (s[x] != '\0')
-		x++;
-	x--;
-	z = x / 2;
-	for (y = 0; x > z; x--)
+int i = 0;
+int j = 0;
+int x;
+	while (*(s + i) != 0)	
+		i++;
+	if ((i % 2) == 0)
+		x = (i / 2);
+	else
+		x = (i / 2) + 1;
+	i--;
+	while (i >= x)
 	{
-		orden = s[x];
-		desorden = s[y];
-		s[y] = orden;
-		s[x] = desorden;
-		y++;
-
+		char c  = *(s + j);
+		*(s + j) = *(s + i);
+		*(s + i) = c;
+		j++;
+		i--;
 	}
 }
