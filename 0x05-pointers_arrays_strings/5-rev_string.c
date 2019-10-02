f9c6d5f958421207708d14981b55c1e07bbde2fa
MachinEmmus <emonsalvep38@gmail.com>
2019-10-02 18:25:27 -0500

    Add notrebloH file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,16 +10,16 @@ void rev_string(char *s)
 int x, y, z;
 x = 0;
 char i, j;
-	while (s)
-		x++
+	while (s[x] != 0)
+		x++;
 	x--;
 	z = x / 2;
 	for (y = 0; x > z; x--)
 	{
 		i = s[x];
 		j = s[y];
-		s[x] = i;
-		s[y] = j;
+		s[y] = i;
+		s[x] = j;
 		y++;
 
 	}
