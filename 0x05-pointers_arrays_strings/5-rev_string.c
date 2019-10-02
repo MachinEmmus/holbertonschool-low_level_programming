5f9948134c5fdecb19c94a34094feb7f841db825
MachinEmmus <emonsalvep38@gmail.com>
2019-10-02 18:38:58 -0500

    no se que pasaaa aqui

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,17 @@ void rev_string(char *s)
 {
 int x, y, z;
 x = 0;
-char i, j;
+char orden, desorden;
 	while (s[x] != '\0')
 		x++;
 	x--;
 	z = x / 2;
 	for (y = 0; x > z; x--)
 	{
-		i = s[x];
-		j = s[y];
-		s[y] = i;
-		s[x] = j;
+		orden = s[x];
+		desorden = s[y];
+		s[y] = orden;
+		s[x] = desorden;
 		y++;
 
 	}
