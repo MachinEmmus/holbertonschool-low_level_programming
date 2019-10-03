d56498fb055cb6784166c11d51b7bba1339bf4bf
MachinEmmus <emonsalvep38@gmail.com>
2019-10-03 15:25:34 -0500

    TASK 0,1 MELOSKY

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+/**
+ * *_strcat - Funtion thats return to char
+ * @dest: "Hello ";
+ * @src: "World!"
+ * Return: Char value of the String
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int a = 0;
+int b = 0;
+	while (*(dest + a) != '\0')
+		a++;
+	while (b <= a && src[b] != src[n])
+	{
+		dest[a + b] = src[b];
+		b++;
+	}
+	dest[a + b] = '\0';
+	return (dest);
+}
