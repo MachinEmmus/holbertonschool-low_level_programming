a15c8bb5d030ab1136b4431b707b5ed8fdedff9b
MachinEmmus <emonsalvep38@gmail.com>
2019-10-15 14:35:09 -0500

    What happend here

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,9 +9,10 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
+char *p;
 	if (size == 0)
 		return (NULL); 
-	char *p = malloc(size * sizeof(char));
+	p = malloc(size * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
