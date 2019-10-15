5d9e96c18aed9b75533130cdb72c1bf625c50d0d
MachinEmmus <emonsalvep38@gmail.com>
2019-10-15 14:22:13 -0500

    Maybe era this

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ unsigned int i;
 	if (size == 0)
 		return (NULL); 
 	char *p = malloc(size * sizeof(char));
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		*(p + i) = c; 
 	return (p);
