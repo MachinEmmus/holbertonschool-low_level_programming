ea49585eace9b3162b6f2e355bb328835388b539
MachinEmmus <emonsalvep38@gmail.com>
2019-10-15 14:45:14 -0500

    add README.md

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 /**
- * *create_array - create array with size, and char specific 
+ * *create_array - create array with size, and char specific
  * @size: Is 98
- * @c: char 
+ * @c: char
  * Return: char of pointer
  */
 
@@ -11,11 +11,11 @@ char *create_array(unsigned int size, char c)
 unsigned int i;
 char *p;
 	if (size == 0)
-		return (NULL); 
+		return (NULL);
 	p = malloc(size * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		*(p + i) = c; 
+		*(p + i) = c;
 	return (p);
 }
