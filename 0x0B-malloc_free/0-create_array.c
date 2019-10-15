860fba898170916570001f85e30135b973aed17b
MachinEmmus <emonsalvep38@gmail.com>
2019-10-15 14:18:32 -0500

    0x0B AND TASK 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * 
+ *
+ *
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+	if (size == 0)
+		return (NULL); 
+	char *p = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+		*(p + i) = c; 
+	return (p);
+}
