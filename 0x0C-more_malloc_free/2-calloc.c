7eef1c4e9efdf93ca7422721671c9e03a76126a6
MachinEmmus <emonsalvep38@gmail.com>
2019-10-17 18:19:59 -0500

    task 2 bug fixes

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * _calloc - calloc function
+ * @nmemb: elementos matriciales 
+ * @size: bytes cada uno
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		*(p + i) = 0;
+	return (p);
+}
