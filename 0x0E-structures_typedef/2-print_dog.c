61f118abfac6a55653798cc6b99c541a1bcbf097
MachinEmmus <emonsalvep38@gmail.com>
2019-10-21 18:41:43 -0500

    Add task 2,3,5

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Print Dogs register
+ * @d: structure dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", (d->name == NULL) ? "(nill)" : (*d).name);
+		printf("Name: %.6f\n", d->age );
+		printf("Name: %s\n", (d->owner == NULL) ? "(nill)" : (*d).owner);
+	}
+
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Liberar a los perros
+ * @d: Pointer to my structure dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owned->owner);
+	}
+}
