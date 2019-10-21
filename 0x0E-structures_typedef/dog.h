e91147ecc1190e89105938ae86c38eb6dc91d174
MachinEmmus <emonsalvep38@gmail.com>
2019-10-21 18:57:54 -0500

    add new dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,16 +9,12 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_h;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+};
 
 #endif
