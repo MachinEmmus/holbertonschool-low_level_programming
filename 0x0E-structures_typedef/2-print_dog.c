e425d41f0c1c78134683c71d9cdc402b60237433
MachinEmmus <emonsalvep38@gmail.com>
2019-10-21 19:15:58 -0500

    betty files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,12 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name == NULL ? "(nill)" : d->name);
-		printf("Age: %.6f\n", d->age );
+		printf("Age: %.6f\n", d->age);
 		printf("Owner: %s\n", d->owner == NULL ? "(nill)" : d->owner);
+	}
+	else
+	{
+
 	}
 
 }
