1b80ed5c00ad9352f9f5da8bbde71615ceba5cd7
MachinEmmus <emonsalvep38@gmail.com>
2019-10-21 23:42:14 -0500

    Task 4 other way

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,65 +1,45 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * _strlen - count string
- * @str: input string
- * Return: string length unsigned integer
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i = 0;
-
-	while (*(str + i) != '\0')
-		i++;
-	return (i);
-}
-/**
- * _strcpy - copies string
- * @new: pointer to new string
- * @from: pointer to string to copy
- * Return: void
- */
-void _strcpy(char *new, char *from)
-{
-	unsigned int i, len = _strlen(from);
-
-	for (i = 0; i <= len; i++)
-		new[i] = from[i];
-}
-/**
- * new_dog - creates new dog type
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
- * Return: pointer to the type of struct dog
- */
+* *new_dog - create new dog
+* @name: name of dog
+* @age: age
+* @owner: owner of dog.
+* Return: new_dog estructur
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int name_size = _strlen(name), owner_size = _strlen(owner);
-	dog_t *snoop;
+	int size1 = 0, size2 = 0, x;
+	dog_t *snopp;
 
-	snoop = malloc(sizeof(dog_t));
-	if (snoop)
+	if (name != NULL && owner != NULL)
 	{
-		snoop->name = malloc(sizeof(char) * (name_size + 1));
-		if (snoop->name)
+		while (*(name + size1))
+			size1++;
+		while (*(owner + size2))
+			size2++;
+		snopp = malloc(sizeof(dog_t));
+		if (snopp == NULL)
+			return (NULL);
+		(*snopp).name = malloc(size1 + 1);
+		if ((*snopp).name == NULL)
 		{
-			snoop->owner = malloc(sizeof(char) * (owner_size + 1));
-			if (snoop->owner)
-			{
-				_strcpy(snoop->name, name);
-				snoop->age = age;
-				_strcpy(snoop->owner, owner);
-				return (snoop);
-			}
-			else
-			{
-				free(snoop->name);
-				free(snoop);
-			}
+			free(snopp);
+			return (NULL);
 		}
-		else
-			free(snoop);
+		(*snopp).owner = malloc(size2 + 1);
+		if ((*snopp).owner == NULL)
+		{
+			free((*snopp).name);
+			free(snopp);
+			return (NULL);
+		}
+		for (x = 0; x <= size1; x++)
+			(*snopp).name[x] = name[x];
+		(*snopp).age = age;
+		for (x = 0; x <= size2; x++)
+			(*snopp).owner[x] = owner[x];
+		return (snopp);
 	}
 	return (NULL);
 }
