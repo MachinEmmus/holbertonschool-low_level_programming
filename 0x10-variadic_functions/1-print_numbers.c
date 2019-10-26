de19d236988c524d0404de6f48e85970e14adcfa
MachinEmmus <emonsalvep38@gmail.com>
2019-10-26 14:39:39 -0500

    NULL in Separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,21 +12,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list list;
 unsigned int i;
-	
-	if (!separator)
-	{
-		
-	}
-	else
-	{
+
 		va_start(list, n);
 		for (i = 0; i < n; i++)
 		{
-			printf("%d",va_arg(list, int));
-			if (i < (n - 1))
+			printf("%d", va_arg(list, int));
+			if (separator != NULL && i < (n - 1))
 				printf("%s", separator);
 		}
 		printf("\n");
 		va_end(list);
-	}
 }
