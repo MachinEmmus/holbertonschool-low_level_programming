79688e109206f1110e30f159d9e9af695c82ed7d
MachinEmmus <emonsalvep38@gmail.com>
2019-10-26 13:05:16 -0500

    Calculator is PRO

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+/**
+ * op_add - Funtion to add
+ * @a: first parametrer
+ * @b: second parametrer
+ * Return: Sum of two parametres
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Funtion to sub
+ * @a: first parametrer
+ * @b: second parametrer
+ * Return: Sub of two parametres
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Funtion to mul
+ * @a: first parametrer
+ * @b: second parametrer
+ * Return: mul of two parametres
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Funtion to div
+ * @a: first parametrer
+ * @b: second parametrer
+ * Return: div of two parametres
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Funtion to mod
+ * @a: first parametrer
+ * @b: second parametrer
+ * Return: mod of two parametres
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
