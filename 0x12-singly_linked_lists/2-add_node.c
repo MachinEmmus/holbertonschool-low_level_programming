50b83860697be3d28a13e887276e52eec9f0ca1e
MachinEmmus <emonsalvep38@gmail.com>
2020-01-18 18:42:44 -0500

    add a new node in first position

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_node - add new node in the first position
+ * @head: pointer to my list
+ * @str: is a new value of name
+ * Return: New node or Null
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	if (!head)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
