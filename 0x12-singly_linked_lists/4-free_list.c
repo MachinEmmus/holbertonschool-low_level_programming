2f67dea45193acd3a8328c635f7e11665af80293
MachinEmmus <emonsalvep38@gmail.com>
2020-01-19 15:25:06 -0500

    free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - free a linked list
+ * @head: pointer to my linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(head);
+}
