143f6cc6c3dab0bb326854d4b82f2c8ceeb56f8a
MachinEmmus <emonsalvep38@gmail.com>
2020-01-19 14:52:32 -0500

    Dont use strlen ;(

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,14 +9,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
+int i = 0;
 	if (!head)
 		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
+	while (str[i])
+		i++;
+
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
