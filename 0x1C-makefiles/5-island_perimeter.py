6024878a7915fbb2f3cacd5e5a33a1a51d5c4b99
Emmus Monsalve <emonsalvep38@gmail.com>
2020-02-21 21:41:12 -0500

    pep8 and other situation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
+    """
+        The amazing Five tas    The amazing Five tas    
+    """
     water = 0
     land = 1
     perimeter = 0
 
     for col, level in enumerate(grid):
         for row, parcel in enumerate(level):
-            if parcel == land:  # Checking only when hitting land
-                    if row == 0 or grid[col][row - 1] == water:  # Looking west
+            if parcel == land:
+                    if row == 0 or grid[col][row - 1] == water:
                         perimeter += 1
                     if (row + 1) == len(level) or grid[col][row + 1] == water:
                         perimeter += 1
-                    if row == 0 or grid[col - 1][row] == water:  # Looking north
+                    if row == 0 or grid[col - 1][row] == water:
                         perimeter += 1
                     if (col + 1) == len(grid) or grid[col + 1][row] == water:
                         perimeter += 1
