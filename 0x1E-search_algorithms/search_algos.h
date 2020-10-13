141cf20e3c7c4b9d31f694e63b00c656e24f48a8
Emmanuel Monsalve <emonsalvep38@gmail.com>
2020-10-13 10:55:29 -0500

    add all headers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -42,14 +42,16 @@ typedef struct skiplist_s
 
 int found(int *array, size_t size, size_t low, size_t high, int value);
 void print_array(int *array, size_t low, size_t high);
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
-
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
 
 #endif/*__SEARCH_ALGOS_H__*/
